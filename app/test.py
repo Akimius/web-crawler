bffcf0df9dc67c68f58882535740805a275853f0
Akim Savchenko <akim@doubledmarketing.com>
2025-12-10 20:12:55 +0200

    delete test file

--- app/test.py
+++
@@ -1,9 +0,0 @@
-class BankAccount:
-    MIN_BALANCE = 100
-
-    def __init__(self, owner, balance=0):
-        self.owner = owner
-        self._balance = balance
-
-    def deposit(self, amount):
-        self._balance += amount
